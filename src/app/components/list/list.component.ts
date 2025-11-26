ed35eb21cdf2e9dedfb19562712947b9fea9ef7b
Roan van der Ploeg <roan@qalybr.nl>
2025-11-26 11:26:10 +0100

    add eslint, add formgroup to list component

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { ListItem } from '../../services/data.interace';
 import { DataService } from '../../services/data.service';
@@ -6,7 +14,12 @@ import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatListModule],
+  imports: [
+    MatListModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -15,9 +28,33 @@ export class ListComponent {
 
   items = signal<ListItem[]>([]);
 
+  listForm = new FormGroup({
+    find: new FormControl(''),
+    input: new FormControl('', Validators.required),
+  });
+
   constructor() {
     this.dataService.getListItems().subscribe((items: ListItem[]) => {
       this.items.set(items);
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.listForm.valid) {
+      return;
+    }
+
+    const value = this.listForm.get('input')?.value ?? '';
+    const name = value.replace(/\s+/g, '-').toLowerCase();
+
+    this.items.update((values: ListItem[]) => {
+      return [
+        ...values,
+        {
+          name,
+          value,
+        },
+      ];
     });
   }
 }
